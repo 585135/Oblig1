22d70b879c53bb12ff3237d51235edf13618f9fc
simon <simon@192.168.0.9>
2020-09-02 17:59:55 +0200

    Lagt til oppgave 6.

---
+++ Oblig1/src/oblig1/Oppgave6.java
@@ -0,0 +1,35 @@
+package oblig1;
+import javax.swing.JOptionPane;
+public class Oppgave6 {
+	
+	
+public static void main(String[] args) 
+{
+	String input = "";
+	int n = 1;
+	int svar = 1;
+	int temp = 1;
+	int orgtall = 0;
+	
+	JOptionPane.showMessageDialog(null, "Skriv inn et heltall du vil vite fakultetet av ");
+	
+	input = JOptionPane.showInputDialog("Heltall", input);
+	n = Integer.parseInt(input);
+	orgtall = n;
+	
+	
+	for (int i = 1; i <= orgtall; i++ )
+	{
+	temp = n;	
+	svar = temp * svar;
+	n--;
+	}
+	
+	
+	JOptionPane.showMessageDialog(null, "Svaret ditt er: "+svar);
+	
+	
+	
+}
+
+}
